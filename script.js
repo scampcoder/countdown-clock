2b3c80f478e6a7b51e4a7520195a04edd6eacf08
Victoria Kee <victoriakee95@gmail.com>
2020-06-15 16:07:44 -0500

    Write main, getTime function

---
+++ script.js
@@ -0,0 +1,14 @@
+function getTimeRemaining(endtime) {
+  const t = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((t/1000) % 60);
+  const minutes = Math.floor((t/1000/60) % 60);
+  const hours = Math.floor((t/(1000 * 60 * 60)) % 24);
+  const days = Math.floor(t/(1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
